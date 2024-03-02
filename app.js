733ff29f94d5872d55501fa64963e8e24616296e
Lu Zara <lucininiz@gmail.com>
2024-03-02 19:08:24 -0300

    render filtered results

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const urlBase = `http://gateway.marvel.com/v1/public/`
 let ts = `ts=1`
 const publicKey = `&apikey=fdaea98b3b23d7071fb62c4a0ffc157f`
 const hash = `&hash=4200c5d912fc28d30525b28769be81e7`
+const recurso = "comics"
 
 
 const getMarvelCharacters = async () => {
@@ -13,31 +14,25 @@ const getMarvelCharacters = async () => {
     return data.data.results
 }
 
-getMarvelCharacters()
-
-const getMarvelComics = async (title) => {
-    let existTitle = title? `&titleStartsWith=${title}` : "  "
-    let url = `${urlBase}comics?${ts}${publicKey}${hash}${existTitle}`
+const getMarvelComics = async (recurso, title) => {
+    let existTitle = title? `&titleStartsWith=${title}` : ""
+    let url = `${urlBase}${recurso}?${ts}${publicKey}${hash}${existTitle}`
     const response = await fetch(url)
     const data = await response.json()
     return data.data.results
 }
 
-getMarvelComics()
-
 
 //FILTERS BY COMIC
-const printComic = async() => {
-    const comics = await getMarvelComics()
+const printComic = async(recurso, title) => {
+    const comics = await getMarvelComics(recurso, title)
     $(".characters-cards").innerHTML = ``
 
 for(let comic of comics){
     $(".characters-cards").innerHTML += `
     <div class="border-neutral-500 rounded-lg p-2">
-    <img class="justify-items-center" src="${comic.thumbnail.path}/portrait_xlarge.${comic.thumbnail.extension}">
+    <img class="justify-items-center" src="${comic.thumbnail.path}/portrait_fantastic.${comic.thumbnail.extension}">
         <p class="font-bold justify-start">${comic.title}</p>
-        <p class="font-semibold">GENERO</p>
-        <p class="font-semibold">Status: <span class="font-light"></span></p>
 </div>
     `
 }
@@ -45,17 +40,16 @@ for(let comic of comics){
 
 
 //FILTERING
-
-$("#input-text-filter").addEventListener("input", () => {
-    $("#btn-search").addEventListener("click", () => {
-    console.log($("#input-text-filter").value);
-    printComic($("#input-text-filter").value)
-
+        $("#input-text-filter").addEventListener("input", () => {
+            printComic(recurso,    $("#input-text-filter").value )
+            
 })
-})
+    
+
+
 
 const initialize = () => {
-    printComic()
+    printComic(recurso, "")
 }
 
 window.addEventListener("load", initialize);
